b423e3c9ae9b47304f800eea58aef34aecf39b56
@argaathallah <argaathallah@gmail.com>
2026-09-08 13:32:46 +0700

    feat Transaction Controller: Controller for Transaction

---
+++ src/main/java/com/auroth/coding/controller/TransactionController.java
@@ -0,0 +1,24 @@
+package com.auroth.coding.controller;
+
+import com.auroth.coding.model.Product;
+import com.auroth.coding.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TransactionController {
+
+
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+
+}
